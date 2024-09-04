30ed5f1aa55c5425631232659cfaf739631b1102
Masaya Yotsuya <s1f102201481@iniad.org>
2024-09-04 17:55:48 +0900

    GoogleCalendarのAPI実装を開始

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('<name>', views.top, name='top'),
     path('<name>/chat', views.chat, name='chat'),
     path('<name>/schedule', views.schedule, name='schedule'),
+    path('calendar_api/', views.calendar_api, name='calendar_api'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-import datetime
+from datetime import datetime, timedelta
+import os
+from myproject.settings import BASE_DIR
 
 # Create your views here.
 name_list = {'tomato': 'トマト'}
@@ -24,3 +26,51 @@ def schedule(request, name):
         'name': name_list[name],
     }
     return render(request, 'myapp/schedule.html', params)
+
+def calendar_api(request):
+    if request.method == 'POST':
+        start_date_str = request.POST.get('start_date')
+        if start_date_str:
+            start_date = datetime.strptime(start_date_str, '%Y-%m-%d').date()
+
+        # カレンダーAPIのスコープを設定
+        SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+        # ユーザー認証を行いAPIクライアントを作成
+        flow = InstalledAppFlow.from_client_secrets_file(os.path.join('BASE_DIR', '/myapp/credentials.json'), SCOPES)
+        creds = flow.run_local_server(port=0)
+
+        service = build('calendar', 'v3', credentials=creds)
+
+        # イベントデータを作成
+        events = [
+            {
+                'summary': '種まき',
+                'description': '種をまきましょう\nexample.com',
+                'start': {
+                    'date': start_date.strftime('%Y-%m-%d'),
+                    'timeZone': 'Asia/Tokyo',
+                },
+                'end': {
+                    'date': (start_date + timedelta(days=1)).strftime('%Y-%m-%d'),
+                    'timeZone': 'Asia/Tokyo',
+                },
+            },
+            {
+                'summary': '追肥',
+                'description': '追肥をしましょう\nexample.com',
+                'start': {
+                    'date': (start_date + timedelta(days=10)).strftime('%Y-%m-%d'),
+                    'timeZone': 'Asia/Tokyo',
+                },
+                'end': {
+                    'date': (start_date + timedelta(days=15)).strftime('%Y-%m-%d'),
+                    'timeZone': 'Asia/Tokyo',
+                },
+            },
+        ]
+
+        # イベントを追加
+        for event in events:
+            created_event = service.events().insert(calendarId='primary', body=event).execute()
+            print('Event created: %s' % (created_event.get('htmlLink'))) 
