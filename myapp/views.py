d110fc67b71684181283542db3564b86be2825d1
Masaya Yotsuya <s1f102201481@iniad.org>
2024-09-04 16:47:41 +0900

    top, chat, scheduleの3ページを作成

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+import datetime
 
 # Create your views here.
+name_list = {'tomato': 'トマト'}
+
+def top(request, name):
+    params = {
+        'name': name,
+    }
+    return render(request, 'myapp/top.html', params)
+
+def chat(request, name):
+    params = {
+        'name': name,
+    }
+    return render(request, 'myapp/chat.html', params)
+
+def schedule(request, name):
+    params = {
+        'name': name_list[name],
+    }
+    return render(request, 'myapp/schedule.html', params)
