b9105c8ddc4bef33fd501d3f542c8d981de2e3b2
Masaya Yotsuya <s1f102201481@iniad.org>
2024-09-05 16:25:39 +0900

    AWS環境でAPIを実装

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('<crop_name_en>/chat', views.chat, name='chat'),
     path('<crop_name_en>/schedule', views.schedule, name='schedule'),
     path('<crop_name_en>/calendar_api/', views.calendar_api, name='calendar_api'),
+    path('oauth2callback/', views.oauth2callback, name='oauth2callback'),
+    path('authorize/', views.authorize, name='authorize'),
 ]
